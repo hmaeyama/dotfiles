aa9239d07373d405ff7041904db6e21b90c6a87b
hmaeyama <better.late.than.never.00@gmail.com>
2017-02-11 14:38:52 +0900

    fix install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,9 @@ DOTFILES_ROOT=$(pwd -P)
 echo "install start"
 
 # make symbolic links
-for src in $(find "$DOTFILES_ROOT" -type f  -name ".*" -not -path "*.git*")
+for src in $(find "$DOTFILES_ROOT" -type f  -name ".*" -maxdepth 1 -not -path "*git")
   do
+    #echo "$src"
     ln -sf "$src" ~/`basename $src`
   done
 
