0ee291c7031c3492229cc494722cc6bbd087cdc4
hmaeyama <better.late.than.never.00@gmail.com>
2017-02-18 14:02:06 +0900

    install.sh: fix ignore setting

--- install.sh
+++ install.sh
@@ -5,10 +5,15 @@ DOTFILES_ROOT=$(pwd -P)
 echo "install start"
 
 # make symbolic links
-for src in $(find "$DOTFILES_ROOT" -maxdepth 1 -type f -name ".*" -not -path "*git")
-  do
-    #echo "$src"
-    ln -sf "$src" ~/`basename $src`
-  done
+for f in .??*
+do
+    [[ "$f" == ".git" ]] && continue
+    [[ "$f" == ".DS_Store" ]] && continue
+    [[ "$f" == ".gitignore" ]] && continue
+
+    #echo "$f"
+    dot_src=$DOTFILES_ROOT/$f
+    ln -sf "$dot_src" ~/`basename $f`
+done
 
 echo "install complete"
